adcdb20dfbd7af946e1fc98b1e3e2dbdca66a669
Namtk214 <namtk214@gmail.com>
2026-05-02 14:37:37 +0000

    perf: add nn.remat gradient checkpointing to DiTBlock for OOM fix on DiT-L/XL

--- src/model.py
+++ src/model.py
@@ -307,8 +307,9 @@ class SelfFlowDiT(nn.Module):
 
         zs = None
         block_summaries = [] if return_block_summaries else None
+        RematDiTBlock = nn.remat(DiTBlock)
         for i in range(self.depth):
-            x = DiTBlock(
+            x = RematDiTBlock(
                 hidden_size=self.hidden_size, 
                 num_heads=self.num_heads, 
                 mlp_ratio=self.mlp_ratio,
